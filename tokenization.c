a2c88b89296e2b470d86f0ed9d07738eebd8791e
thfranco <thaisfranco19@gmail.com>
2024-06-03 15:34:20 -0300

    get env variable

---
+++ tokenization.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenization.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thfranco <thfranco@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 11:28:37 by thfranco          #+#    #+#             */
+/*   Updated: 2024/06/03 14:39:53 by thfranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenization.h"
+
+void	tokenization(char **av_to_parse)
+{
+	int	i;
+
+	i = 1;
+	while (av_to_parse[i])
+		check_kind(av_to_parse[i])
+}
