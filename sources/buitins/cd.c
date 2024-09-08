13232ac83c41b76cf00715b6efbfea76d8bb2a3d
Luíza <luizafuruno@gmail.com>
2024-09-07 21:04:56 -0300

    Fix pwd and oldpwd envs

--- sources/buitins/cd.c
+++ sources/buitins/cd.c
@@ -15,11 +15,42 @@
 void    update_pwd(t_main *main)
 {
 	char	pwd[PATH_MAX];
+    t_env_node  *temp;
+    int old_pwd;
+    char    *oldpwd;
+    char    *pwd_line;
 
+    old_pwd = 0;
+    temp = main->env;
 	getcwd(pwd, sizeof(pwd));
     free(main->old_pwd);
 	main->old_pwd = main->pwd;
     main->pwd = ft_strdup(pwd);
+    oldpwd = ft_strjoin("OLDPWD=", main->old_pwd);
+    pwd_line = ft_strjoin("PWD=", main->pwd);
+    while (temp != NULL)
+    {
+        if (!ft_strcmp(temp->name_env, "PWD"))
+        {
+            free(temp->value_env);
+			free(temp->line_env);
+            temp->value_env = ft_strdup(main->pwd);
+            temp->line_env = ft_strdup(pwd_line);
+        }
+        else if (!ft_strcmp(temp->name_env, "OLDPWD"))
+        {
+            free(temp->value_env);
+			free(temp->line_env);
+            temp->value_env = ft_strdup(main->old_pwd);
+            temp->line_env = ft_strdup(oldpwd);
+            old_pwd = 1;
+        }
+        temp = temp->next;
+    }
+    if (!old_pwd)
+        append_env_node(&main->env, oldpwd);
+    free(oldpwd);
+    free(pwd_line);
 }
 
 int ft_cd(char **token, t_main *main)
